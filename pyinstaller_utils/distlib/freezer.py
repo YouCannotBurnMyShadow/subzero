ba22b5ed84c5beda77500d7a607da044cacf37cd
xoviat <email@system.com>
2016-12-11 20:47:02 -0600

    Add spec template.

--- pyinstaller_utils/distlib/freezer.py
+++ pyinstaller_utils/distlib/freezer.py
@@ -57,6 +57,41 @@ MSVCR_MANIFEST_TEMPLATE = """
 </assembly>
 """
 
+PYINSTALLER_SPEC_TEMPLATE = """
+from pyinstaller_utils import Entrypoint
+
+block_cipher = None
+
+a = Entrypoint('{project_name}', 'console_scripts', '{entry_point_name}',
+             pathex=[SPECPATH] + {pathex},
+             binaries=None,
+             datas={datas},
+             hiddenimports=[],
+             hookspath=[],
+             runtime_hooks=[],
+             excludes=[],
+             win_no_prefer_redirects=False,
+             win_private_assemblies=False,
+             cipher=block_cipher)
+pyz = PYZ(a.pure, a.zipped_data,
+          cipher=block_cipher)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=True,
+          name={project_name},
+          debug=False,
+          strip=False,
+          upx=True,
+          console=False,
+          icon={project_icon})
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=False,
+               upx=True,
+               name={project_name})
+"""
 
 def process_path_specs(specs):
     """Prepare paths specified as config.
