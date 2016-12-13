dbe7445e461ebf9e8ff75c7848d997354fa9a4aa
xoviat <email@system.com>
2016-12-13 08:49:09 -0600

    Add test package.

---
+++ tests/hello_world/hello_world/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    input("Press Enter to continue...")
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/hello_world/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+
+from pyinstaller_utils.dist import setup
+
+setup(name='hello_world',
+      version='0.1.0',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'my_project = hello_world.__main__:main'
+          ]
+      },
+      )
