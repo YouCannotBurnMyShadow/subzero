e763bc5fc3f592f5144a3188f3b1d7a7d805ddef
xoviat <email@system.com>
2016-12-14 22:15:42 -0600

    Cleanup setup and add tests.

---
+++ tests/test_setup.py
@@ -0,0 +1,11 @@
+import os
+import subprocess
+
+
+def test_hello_world():
+    package_path = os.path.join(os.path.dirname(__file__), 'hello_world')
+    os.chdir(package_path)
+
+    return_code = subprocess.call(['python', 'setup.py', 'build_exe'])
+
+    assert return_code == 0
