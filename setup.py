1926a84c57f6f06df3605e09020850dd958406a1
xoviat <xoviat@users.noreply.github.com>
2016-12-22 14:06:41 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ import os
 from setuptools import setup, find_packages
 
 BASE_DIR = os.path.dirname(__file__)
-README_PATH = os.path.join(BASE_DIR, 'README.md')
 VERSION_PATH = os.path.join(BASE_DIR, 'VERSION')
 LICENSE_PATH = os.path.join(BASE_DIR, 'LICENSE')
-DESCRIPTION = open(README_PATH).read()
 VERSION = open(VERSION_PATH).read()
 LICENSE = open(LICENSE_PATH).readline().strip()
 
@@ -16,7 +14,6 @@ setup(
     name='pyinstaller_utils',
     version=VERSION,
     description='PyInstaller setuptools integration',
-    long_description=DESCRIPTION,
     author='Mars Galactic',
     author_email='xoviat@users.noreply.github.com',
     url='https://github.com/xoviat/pyinstaller_utils',
@@ -26,4 +23,5 @@ setup(
     keywords=['pyinstaller'],
     classifiers=[],
     install_requires=['PyInstaller', 'PyRTF3', 'packaging'],
-    )
+    setup_requires=['setuptools-markdown'],
+    long_description_markdown_filename='README.md')
