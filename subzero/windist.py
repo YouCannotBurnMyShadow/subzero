3e7c9683b20d7ff5b3f74d48d8e3d7a9261d46be
xoviat <xoviat@users.noreply.github.com>
2017-03-28 16:37:12 -0500

    windist: remove rtf code.

--- subzero/windist.py
+++ subzero/windist.py
@@ -10,8 +10,6 @@ import uuid
 import textwrap
 from io import StringIO
 
-import PyRTF
-import lxml.etree as le
 import pywix
 from pkg_resources import resource_filename, resource_string
 
@@ -48,59 +46,6 @@ class bdist_msi(distutils.command.bdist_msi.bdist_msi):
         folders.reverse()
 
         return folders
-
-    def _license_text(self, license_file):
-        """
-        Generates rich text given a license file-like object
-        :param license_file: file-like object
-        :return:
-        """
-        wordpad_header = textwrap.dedent(r'''
-            {\rtf1\ansi\ansicpg1252\deff0\nouicompat\deflang1033{\fonttbl{\f0\fnil\fcharset255 Times New Roman;}
-            {\*\generator Riched20 10.0.14393}\viewkind4\uc1
-            ''').strip().replace('\n', '\r\n')
-        center_space = '            '
-
-        r = PyRTF.Renderer()
-
-        doc = PyRTF.Document()
-        ss = doc.StyleSheet
-        sec = PyRTF.Section()
-        doc.Sections.append(sec)
-
-        is_blank = False
-        paragraph_text = ['']
-        for line in license_file:
-            if not line or line.isspace():
-                is_blank = True
-            if is_blank:
-                # first element of paragraph_text is left-aligned, subsequent elements are centered
-                is_centered = False
-                for sec_line in paragraph_text:
-                    if is_centered:
-                        para_props = PyRTF.ParagraphPS(
-                            alignment=PyRTF.ParagraphPS.CENTER)
-                        p = PyRTF.Paragraph(ss.ParagraphStyles.Normal,
-                                            para_props)
-                        p.append(sec_line)
-                        sec.append(p)
-                    elif sec_line:  # first element may be nothing, but not whitespace
-                        sec.append(sec_line)
-                    is_centered = True
-                is_blank = False
-                paragraph_text = ['']
-            if line.startswith(center_space):
-                paragraph_text.append(line.strip())
-                is_blank = True
-            else:
-                paragraph_text[0] += ' ' + line
-                paragraph_text[0] = paragraph_text[0].strip()
-
-        f = StringIO()
-        f.write(wordpad_header)
-        r.Write(doc, f)
-
-        return f.getvalue()
 
     def finalize_options(self):
         distutils.command.bdist_msi.bdist_msi.finalize_options(self)
